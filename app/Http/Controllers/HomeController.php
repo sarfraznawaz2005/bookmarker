8def8d9d777094fc5930135d3c64c80f0865c820
Sarfraz <sarfraznawaz2005@gmail.com>
2016-07-14 22:58:35 +0500

    added annotate button on dashboard page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,20 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\Models\Bookmark;
 
 class HomeController extends Controller
 {
     /**
      * Show the application dashboard.
      *
+     * @param Bookmark $bookmark
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Bookmark $bookmark)
     {
         $title = 'Dashboard';
 
         $folders = auth()->user()->folders()->with('bookmarks')->get();
 
-        return view('pages.home', compact('title', 'folders'));
+        // first bookmark to start annotation from for this folder
+        $bookmark::$orderDirection = 'DESC';
+
+        $bookmark = $bookmark
+            ->where('user_id', auth()->user()->id)
+            ->limit(1)
+            ->first();
+
+        return view('pages.home', compact('title', 'folders', 'bookmark'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,36 +2,51 @@
 
 @section('content')
 
-    <div id="colors">
-        <strong>Colors:</strong>
-        <strong class="yellow">01+</strong>
-        <strong class="blue">25+</strong>
-        <strong class="green">50+</strong>
-    </div>
-    <hr>
+    <div class="whitebox">
 
-    @if(! count($folders))
-        <div align="center" class="whitebox">
-            <strong class="alert alert-info"><i class="glyphicon glyphicon-info-sign"></i> No bookmarks saved yet :-(
-            </strong>
+        <div style="margin: 0 16px;">
+            <div class="pull-left">
+                <a target="_blank" href="{{url('/')}}/annotate/{{$bookmark->id}}" class="btn btn-info">
+                    <b class="glyphicon glyphicon-book"></b> Annotate Bookmarks
+                </a>
+
+            </div>
+
+            <div class="pull-right">
+                <div id="colors">
+                    <strong>Colors:</strong>
+                    <strong class="yellow">01+</strong>
+                    <strong class="blue">25+</strong>
+                    <strong class="green">50+</strong>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <hr>
         </div>
-    @endif
 
-    @foreach($folders as $folder)
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12" style="margin: 13px 0;">
-            <a href="{{url('/show_folder/' . $folder->id)}}">
-                <div class="pricing-table {{getFolderColor($folder)}}">
-                    <div class="pt-header">
-                        <div class="plan-pricing">
-                            <div class="pricing">{{count($folder->bookmarks)}}</div>
+        @if(! count($folders))
+            <div align="center" class="whitebox">
+                <strong class="alert alert-info"><i class="glyphicon glyphicon-info-sign"></i> No bookmarks saved yet
+                    :-(
+                </strong>
+            </div>
+        @endif
+
+        @foreach($folders as $folder)
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12" style="margin: 13px 0;">
+                <a href="{{url('/show_folder/' . $folder->id)}}">
+                    <div class="pricing-table {{getFolderColor($folder)}}">
+                        <div class="pt-header">
+                            <div class="plan-pricing">
+                                <div class="pricing">{{count($folder->bookmarks)}}</div>
+                            </div>
+                        </div>
+                        <div class="pt-body">
+                            <h4>{{$folder->name}}</h4>
                         </div>
                     </div>
-                    <div class="pt-body">
-                        <h4>{{$folder->name}}</h4>
-                    </div>
-                </div>
-            </a>
-        </div>
-    @endforeach
-
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endsection
